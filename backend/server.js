7438707e83d17286c721e80580b3807b12bcee7a
Mehroz farooq <85057779+Mehroz101@users.noreply.github.com>
2024-12-04 12:31:20 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 app.use("/uploads", express.static("uploads")); // Serve uploaded files
-
 const io = socketIo(server, {
   cors: {
     origin: "*",
